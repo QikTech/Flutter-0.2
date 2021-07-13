6914e8c7b3461f1fbffdd311b6366110215b73aa
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-13 10:14:01 +0000

    //STRING INTERPOLATION

--- main.dart
+++ main.dart
@@ -1,7 +1,15 @@
 void main() {
-	print('There Once was a man named Ketan');
-	print('He was 24 years old');
-	print('He really liked the name ketan');
-	print("But he didn't like being 70");
+
+//STRING INTERPOLATION
+String characterName = "Ketan";
+String characterAge = "24";
+
+	print('There Once was a man named ${characterName}');
+	print('He was ${characterAge} years old');
+
+  characterName = "prasanna";
+
+	print('He really liked the name ${characterName}');
+	print("But he didn't like being ${characterAge}");
 
 }
