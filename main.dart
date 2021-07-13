55ee6e6788281702bcc309513b14a434b091c100
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-13 12:02:45 +0000

    LIST BASICS

--- main.dart
+++ main.dart
@@ -2,25 +2,41 @@ import "dart:math";
 import "dart:io";
 
 void main() {
+//STROING MULTIPLE PEACES OF DATA
 
-print('Roses are red');
-print('Skies are blue');
-print('i love you');
+// LIST/ARRAY
+ List<int> favNums = [2, 4, 6, 8, 10];
+ //                   0  1  2  3  4
 
-//MADLIB GAME
-print('Enter color');
-String color = stdin.readLineSync();
+ print(favNums);
+ print(favNums[1]);
 
-print('Enter noun');
-String noun = stdin.readLineSync();
+ favNums[1]=44; //changing value 
+ print(favNums);
+ print(favNums[1]);
 
-print('Enter celebrity');
-String celebrity = stdin.readLineSync();
+ print(favNums[1]+favNums[3]); //addition of two elements in LIST
 
-print('--------');
-print('Roses are ${color}');
-print('${noun} are blue');
-print('i love ${celebrity}');
+ //ADDING ELEMENT TO ARRAY
+ favNums.add(66);
+ print('After adding new element ${favNums}');
+
+ favNums.remove(66);
+ print('After removing new element ${favNums}');
+
+  favNums.remove([1]);
+ print('After removing new element ${favNums}');
+
+ print(favNums.indexOf(10));
+ 
+ print(favNums.contains(1));
+ print(favNums.contains(2));
+
+
+
+ //REMOVING ELEMENT TO ARRAY
+ //ADDING ELEMENT TO ARRAY
+ //ADDING ELEMENT TO ARRAY
 
 
 
