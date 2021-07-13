832cb1a3285eda1afa588f193482c2706b10016a
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-13 10:37:55 +0000

    Working With Strings

--- main.dart
+++ main.dart
@@ -1,18 +1,45 @@
 void main() {
 
-//STRING DataType
-String firstName = "Ketan";
+String greetings = "HellO";
+//                  01234
 
-//Number DataType
-int age = 24;
-double gpa=6.5;
+print(greetings);
+print("above string includes ${greetings.length} characters");
 
-//Boolean DataType
-bool isAbove18 = false;
 
-print(firstName);
-print(gpa);
-print(isAbove18);
+print('---------');
+print(greetings[0]);
+print(greetings[1]);
+print(greetings[2]);
+print(greetings[3]);
+print(greetings[4]);
+
+print('---------');
+print(greetings.toUpperCase());
+print(greetings.toLowerCase());
+
+print('--------- index position of given character');
+print(greetings.indexOf("e"));
+
+print('--------- checks if contains that character');
+print(greetings.contains("e"));
+print(greetings.contains('p'));
+
+print('--------- String Concatenation');
+String firstName='prasad';
+String lastName=' sawant';
+print(firstName+lastName);
+print(lastName[0]);
+
+print('--------- String Interpolation');
+print("And his name is ${firstName.toUpperCase()+lastName.toUpperCase()}");
+
+
+
+
+
+
+
 
 
 
