4c1eb1a383c974c298402fc6b76fb73eca47b914
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-13 10:05:20 +0000

    Print Statements

--- main.dart
+++ main.dart
@@ -1,6 +1,7 @@
 void main() {
-	print('   /|');
-	print('  / |');
-	print(' /  |');
-	print('/---|');
+	print('There Once was a man named Ketan');
+	print('He was 24 years old');
+	print('He really liked the name ketan');
+	print("But he didn't like being 70");
+
 }
