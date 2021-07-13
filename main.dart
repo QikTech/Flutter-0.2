a67db9a47a2c43f053a4f10406e5fdbb84fa4239
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-13 12:11:29 +0000

    FUNCTIONS INTRO

--- main.dart
+++ main.dart
@@ -1,44 +1,36 @@
 import "dart:math";
 import "dart:io";
 
+void drawTriangle(){
+  print(' /|');
+  print('/_|');
+}
+
+//FUNCTIONS
 void main() {
-//STROING MULTIPLE PEACES OF DATA
+  print(' /|');
+  print('/.|');
 
-// LIST/ARRAY
- List<int> favNums = [2, 4, 6, 8, 10];
- //                   0  1  2  3  4
+  print(' /|');
+  print('/.|');
 
- print(favNums);
- print(favNums[1]);
+  print(' /|');
+  print('/.|');
 
- favNums[1]=44; //changing value 
- print(favNums);
- print(favNums[1]);
+  print(' /|');
+  print('/.|');
 
- print(favNums[1]+favNums[3]); //addition of two elements in LIST
+  print(' /|');
+  print('/.|');
 
- //ADDING ELEMENT TO ARRAY
- favNums.add(66);
- print('After adding new element ${favNums}');
-
- favNums.remove(66);
- print('After removing new element ${favNums}');
-
-  favNums.remove([1]);
- print('After removing new element ${favNums}');
-
- print(favNums.indexOf(10));
- 
- print(favNums.contains(1));
- print(favNums.contains(2));
+  print('----------With function');
 
 
-
- //REMOVING ELEMENT TO ARRAY
- //ADDING ELEMENT TO ARRAY
- //ADDING ELEMENT TO ARRAY
-
-
+  drawTriangle();
+  drawTriangle();
+  drawTriangle();
+  drawTriangle();
+  drawTriangle();
 
 
 }
