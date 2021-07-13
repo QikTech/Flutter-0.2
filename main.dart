9b22ecfafd24f80970c9fc1a214520f532bde982
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-13 12:45:34 +0000

    FUNCTIONS IN DETAILS

--- main.dart
+++ main.dart
@@ -1,36 +1,38 @@
 import "dart:math";
 import "dart:io";
 
-void drawTriangle(){
-  print(' /|');
-  print('/_|');
+//FUNCTION 
+//PARAMETERS AND ARGUMENTS
+void sayHi(){
+print('hi Prasad!');
 }
 
-//FUNCTIONS
+void paraHi(String username){
+  print('hi ${username}!');
+}
+
+void para2Hi(String username, int age){
+  print('hi ${username}!, you are${age} nice.');
+}
+
+double addNumbers(double num1, double num2){
+  return num1+num2;
+  print('after return'); //use return statement at end
+
+}
+
 void main() {
-  print(' /|');
-  print('/.|');
 
-  print(' /|');
-  print('/.|');
+  sayHi();
+  paraHi('Ketan');
+  para2Hi('prasanna',23);
 
-  print(' /|');
-  print('/.|');
+  //just calling a return statement function
+  addNumbers(5,10);
+  //printing return statement function
+  print(addNumbers(5,10));
 
-  print(' /|');
-  print('/.|');
-
-  print(' /|');
-  print('/.|');
-
-  print('----------With function');
-
-
-  drawTriangle();
-  drawTriangle();
-  drawTriangle();
-  drawTriangle();
-  drawTriangle();
-
-
+  //Declaring variable with a parameterized function
+  double answer = addNumbers(20,30);
+  print(answer);
 }
