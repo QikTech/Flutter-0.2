35778501e1f096929e4d0c7a393fba5d81410d79
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-13 11:03:08 +0000

    INPUT FROM USER

--- main.dart
+++ main.dart
@@ -1,13 +1,18 @@
 import "dart:math";
+import "dart:io";
 
 void main() {
+  //print('what is your name:');
+  //String userName= stdin.readLineSync(); // //TAKE INPUT FROM USER
+  //print('hey ${userName}');
 
-//COMPARISON/CONDITIONAL OPERATORS
-print(5>3);
-print(5<3);
-print(5<=3);
-print(5>=3);
+  print('Enter First number:');
+  String num1=stdin.readLineSync();
 
+  print('Enter Second number:');
+  String num2=stdin.readLineSync();
 
-
+  print(
+    int.parse(num1)+int.parse(num2)
+  );
 }
