d03eb1271f6be8e648ca4fd196980f37779700fe
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-13 13:14:56 +0000

    FUNCTIONS EXERCISE (REUSABLITY)

--- main.dart
+++ main.dart
@@ -1,38 +1,23 @@
 import "dart:math";
 import "dart:io";
 
-//FUNCTION 
-//PARAMETERS AND ARGUMENTS
-void sayHi(){
-print('hi Prasad!');
-}
-
-void paraHi(String username){
-  print('hi ${username}!');
-}
-
-void para2Hi(String username, int age){
-  print('hi ${username}!, you are${age} nice.');
-}
-
-double addNumbers(double num1, double num2){
-  return num1+num2;
-  print('after return'); //use return statement at end
-
+String prompt(String promptText){
+  print(promptText);
+  String answer = stdin.readLineSync();
+  return answer;
 }
 
 void main() {
 
-  sayHi();
-  paraHi('Ketan');
-  para2Hi('prasanna',23);
+String color = prompt('Enter a color:');
+String pluralNoun = prompt('Enter a noun:');
+String celebrity = prompt('Enter a celebrity:');
 
-  //just calling a return statement function
-  addNumbers(5,10);
-  //printing return statement function
-  print(addNumbers(5,10));
+print('---------');
+print('the color is ${color}');
+print('the color is ${pluralNoun}');
+print('the color is ${celebrity}');
 
-  //Declaring variable with a parameterized function
-  double answer = addNumbers(20,30);
-  print(answer);
+
 }
+
