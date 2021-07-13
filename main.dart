f8a2474bfabc13b86c1ff8616a699aeca74cd44e
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-13 10:53:10 +0000

    COMPARISONS WITH BOOLEANS

--- main.dart
+++ main.dart
@@ -2,25 +2,12 @@ import "dart:math";
 
 void main() {
 
-int quantity=300;
-double price=5.99;
+//COMPARISON/CONDITIONAL OPERATORS
+print(5>3);
+print(5<3);
+print(5<=3);
+print(5>=3);
 
-//SHORTHAND +=
-quantity=quantity+50;
-quantity+=50; // /*-+
-print(quantity);
-//Increment SHORTHAND
-quantity++;
-print(quantity);
-
-//print(5+3*(2/7));
-print(10/3); //divide operator 
-print(10%3); //modular operator 
-
-print("------FROM MATH LIABRARY");
-print(min(1,9));
-print(max(1,9));
-print(sqrt(144)); //SQUARE ROOT
 
 
 }
