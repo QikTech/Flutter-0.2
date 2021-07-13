f263800dba6bc8babb2da195fdcb4f8f506108e5
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-13 13:46:43 +0000

    IF ELSE INTRO

--- main.dart
+++ main.dart
@@ -1,23 +1,62 @@
 import "dart:math";
 import "dart:io";
 
+
+
+void main() {
+// example 1
+  bool isSmart = true;
+  if(isSmart){
+    print('You are Smart');
+  }
+  else {
+  print('You should learn more');
+  }
+
+// example 2
+  String greeting = 'hello';
+  if(greeting.contains('e')){
+    print('Greetings has e in it');
+  }
+  else {
+    print('E not found');
+  }
+
+// SHORTHAND FOR IF ELSE EXAMPLE 2 (ONLY FOR SINGLE CONDITION)
+  if(greeting.contains('e')) print('Greetings has e in it');
+  
+  else print('E not found');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 String prompt(String promptText){
   print(promptText);
   String answer = stdin.readLineSync();
   return answer;
 }
-
-void main() {
-
-String color = prompt('Enter a color:');
-String pluralNoun = prompt('Enter a noun:');
-String celebrity = prompt('Enter a celebrity:');
-
-print('---------');
-print('the color is ${color}');
-print('the color is ${pluralNoun}');
-print('the color is ${celebrity}');
-
-
-}
-
