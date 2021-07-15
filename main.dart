15fcb91db4efc3a239db0af7a19bbb7eb52174a0
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-15 11:30:31 +0000

    If Else - Simple Calculator

--- main.dart
+++ main.dart
@@ -1,69 +1,38 @@
 import "dart:math";
 import "dart:io";
 
-//COMPARRISON OPERATORS
-void main() {
-
-  bool isSmart = true;
-  bool isStudent = false;
-
-//AND - BOTH CONDITIONAL STATEMENTS NEEDS TO BE TRUE
-  if(isSmart && isStudent){
-    print('1. You are a Smart Student');
-  }
-
-
-// OR - OPERATOR ie.|| ONLY ONE STATEMENT NEED TO BE TRUE, ***ONLY TRUE REQ***
-if(isSmart || isStudent){ // or operator
-  print('2. You are either Smart or a Student or both');
-}
-
-  if(isSmart && isStudent){
-    print('3. You are a smart student');
-  } else if (isSmart && !isStudent){
-    print('4.you are a smart but not a Student');
-  }
-
-  if(isSmart && isStudent){
-    print('5. You are a smart student');
-  } else if (isSmart && !isStudent){
-    print('6.you are a smart but not a Student');
-  }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 String prompt(String promptText){
-  print(promptText);
+  print("The prompt is: ${promptText}");
   String answer = stdin.readLineSync();
   return answer;
+}
+
+double promptDouble(){
+  print('Enter a number: ');
+  double myNym = double.parse(stdin.readLineSync());
+  return myNym;
+}
+
+void main() {
+  double num1=promptDouble();
+  double num2=promptDouble();
+  String output= prompt("Enter an operation (+-/*): ");
+
+  if(output == '+'){
+    print(num1+num2);
+  }
+  else if (output == '-'){
+    print(num1-num2);
+  }
+  else if (output == '/'){
+    print(num1/num2);
+  }
+  else if (output == '*'){
+    print(num1*num2);
+  }
+  else{
+    print('Incorrect Math operation, Try Again');
+  }
+
 }
