10c8d18394e63017bc7a93c2413fa5b7e8324d66
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-18 13:44:48 +0000

    Class with Constructor

--- main.dart
+++ main.dart
@@ -2,6 +2,15 @@ import "dart:io";
 
 // Class
 class Book {
+
+  //CONSTRUCTOR FUNCTION
+  Book(String aTitle, String aAuthor, int aNoOfPages){
+    this.numOfPages=aNoOfPages;
+    this.author=aAuthor;
+    this.title=aTitle;
+  }
+  //CONSTRUCTOR FUNCTION
+
   String title;
   String author;
   int numOfPages;
@@ -10,11 +19,12 @@ class Book {
 
 
 void main() {
-  Book hp=Book(); //object of class
-
-  hp.title='Prisner of Azkban';
-  hp.author='jk Rowling';
-  hp.numOfPages=500;
+  
+  //Book No 1
+  Book hp=Book('Prisner of Azkban','jk',500); //object of class
+  //hp.title='Prisner of Azkban';
+  //hp.author='jk Rowling';
+  //hp.numOfPages=500;
 
   print(hp.title);
   print(hp.author);
